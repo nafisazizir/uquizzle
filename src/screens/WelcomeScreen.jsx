844743540811c058462faf370190ae4368474ac3
azrahisman <azrahisman@gmail.com>
2024-08-18 01:22:14 +1000

    Removed unnecessary files

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -5,13 +5,6 @@ const WelcomeScreen = ({ onNavigate }) => {
   return (
     <div className="screen-container">
         <Welcome onNavigate={onNavigate} />
-      {/* <h2>Welcome to UQuizzle</h2>
-      {lectureTitle && <h3>{lectureTitle}</h3>}
-      <button onClick={handleTranscribe}>Transcribe Lecture</button>
-      <div className="navigation-buttons">
-        <button onClick={() => onNavigate('quiz')}>Go to Quiz</button>
-        <button onClick={() => onNavigate('notes')}>Go to Notes</button>
-      </div> */}
     </div>
   );
 };
