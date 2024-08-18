c3d917fc71e829577e46b3385e2209ce16bb3a81
Matthew-Rizky-Hartadi <matthew.rizky11@ui.ac.id>
2024-08-18 13:01:30 +1000

    resolve styling conflict

--- src/screens/QuizScreen.jsx
+++ src/screens/QuizScreen.jsx
@@ -164,12 +164,13 @@ const QuizScreen = ({ transcriptText, onNavigate, lectureTitle }) => {
             <button className="review-button" onClick={() => onNavigate("notes")}>
               Review Lecture Notes
             </button>
-            <button className="download-button" onClick={handleDownloadQuizzes}>
+            <button className="download-button" onClick={handleDownloadQuizzes} style={{backgroundColor:"white", color:"rgba(213, 1, 108, 1)", border: "2px solid var(--strawbz) !important" }}>
               Download Quizzes
             </button>
             <button
               className="dashboard-button"
               onClick={handleBackToDashboard}
+              style={{backgroundColor:"white", color:"rgba(213, 1, 108, 1)" }}
             >
               Back to Dashboard
             </button>
